0ab705c258d681195caabbf64e9018cda5768b3f
Padmini Kumari P <113809784+PadminiP2603@users.noreply.github.com>
2023-02-03 09:32:46 +0530

    Update cart.jsx

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -18,7 +18,7 @@ const Cart = () => {
 
   const createOrder = async (data) => {
     try {
-      const res = await axios.post("http://localhost:3000/api/orders", data);
+      const res = await axios.get("http://localhost:3000/api/products");
       if (res.status === 201) {
         dispatch(reset());
         router.push(`/orders/${res.data._id}`);
