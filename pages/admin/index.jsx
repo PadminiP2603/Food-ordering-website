ed8276fd97e8793c86701f35e43634972290c9f9
Padmini Kumari P <113809784+PadminiP2603@users.noreply.github.com>
2023-02-03 09:28:06 +0530

    Update index.jsx

--- pages/admin/index.jsx
+++ pages/admin/index.jsx
@@ -12,7 +12,7 @@ const Index = ({ orders, products }) => {
     console.log(id);
     try {
       const res = await axios.delete(
-        "https://react-food-website-beryl.vercel.app" + id
+        "http://localhost:3000/api/products/" + id
         
       );
       setPizzaList(pizzaList.filter((pizza) => pizza._id !== id));
@@ -26,7 +26,7 @@ const Index = ({ orders, products }) => {
     const currentStatus = item.status;
 
     try {
-      const res = await axios.put("https://react-food-website-beryl.vercel.app" + id, {
+      const res = await axios.put("http://localhost:3000/api/orders/" + id, {
         status: currentStatus + 1,
       });
       setOrderList([
@@ -130,9 +130,9 @@ export const getServerSideProps = async (ctx) => {
     };
   }
 
- 
-  const productRes = await axios.get("https://react-food-website-beryl.vercel.app/");
-  const orderRes = await axios.get("https://react-food-website-beryl.vercel.app/");
+  const productRes = await axios.get("http://localhost:3000/api/products");
+  const orderRes = await axios.get("http://localhost:3000/api/orders");
+  
 
   return {
     props: {
